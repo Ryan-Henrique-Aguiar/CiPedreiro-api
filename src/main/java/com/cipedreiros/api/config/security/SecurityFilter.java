56386d78f124b200cdbd33a3b89e576c65b2ce14
ryan <ryan.aguiarh@gmail.com>
2026-02-13 12:00:12 -0300

    refactor: add excpetion in securityFilter

--- src/main/java/com/cipedreiros/api/config/security/SecurityFilter.java
+++ src/main/java/com/cipedreiros/api/config/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package com.cipedreiros.api.config.security;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.cipedreiros.api.repository.UsersRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -28,18 +29,43 @@ public class SecurityFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if (token != null){
-            var email = tokenService.validateToken(token);
-            UserDetails user = usersRepository.findByEmail(email);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user,null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
+        if (token != null &&  SecurityContextHolder.getContext().getAuthentication() == null) {
+            try {
+                var email = tokenService.validateToken(token);
+                UserDetails user = usersRepository.findByEmail(email);
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            user.getAuthorities()
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }catch(JWTVerificationException e){
+                    sendUnauthorized(response,"Token invalido ou expirado");
+                    return;
+                }
+            }
         filterChain.doFilter(request, response);
     }
     private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
         if(authHeader == null) return null;
         return authHeader.replace("Bearer ","");
+    }
+
+    private void sendUnauthorized(HttpServletResponse response, String message) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String json = """
+                {
+                  "error": "%s"
+                }
+                """.formatted(message);
+
+        response.getWriter().write(json);
     }
 }
